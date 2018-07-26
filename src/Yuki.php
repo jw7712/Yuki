e58a78d303aef1429c15a5e91b501e9902659bf1
Joris Debonnet <joris@funkytime.com>
2018-07-27 01:10:16 +0200

    Some PSR-2 improvements and some typos

--- src/Yuki.php
+++ src/Yuki.php
@@ -63,39 +63,50 @@ class Yuki
      */
     public function ProcessInvoice($invoice, $escaped = false) {
         // This currently assumes that all array values are properly escaped
-        if(!$escaped) {
+        if (!$escaped) {
             // todo
         }
 
         // General fields
         $SalesInvoice = '';
-        foreach(['Reference', 'Subject', 'PaymentMethod', 'Date', 'DueDate', 'Currency', 'ProjectCode', 'Remarks'] as $k) {
-            if(!empty($invoice[$k])) $SalesInvoice .= "<$k>$invoice[$k]</$k>";
-            if($k === 'PaymentMethod') $SalesInvoice .= '<Process>true</Process>'; // invoice is marked as fully prepared
+        foreach (['Reference', 'Subject', 'PaymentMethod', 'Date', 'DueDate', 'Currency', 'ProjectCode', 'Remarks'] as $k) {
+            if (!empty($invoice[$k])) {
+                $SalesInvoice .= "<$k>$invoice[$k]</$k>";
+            }
+            if ($k === 'PaymentMethod') {
+                // invoice is marked as fully prepared
+                $SalesInvoice .= '<Process>true</Process>';
+            }
         }
 
         // Client (company)
         $Contact = '';
-        foreach(['ContactCode', 'FullName', 'CountryCode', 'City', 'Zipcode', 'AddressLine_1', 'AddressLine_2', 'EmailAddress', 'CoCNumber', 'VATNumber', 'ContactType'] as $k) {
-            if(!empty($invoice['Contact'][$k])) $Contact .= "<$k>{$invoice['Contact'][$k]}</$k>";
+        foreach (['ContactCode', 'FullName', 'CountryCode', 'City', 'Zipcode', 'AddressLine_1', 'AddressLine_2', 'EmailAddress', 'CoCNumber', 'VATNumber', 'ContactType'] as $k) {
+            if (!empty($invoice['Contact'][$k])) {
+                $Contact .= "<$k>{$invoice['Contact'][$k]}</$k>";
+            }
         }
         $SalesInvoice .= "<Contact>$Contact</Contact>";
 
-        if(!empty($invoice['ContactPerson']) && !empty($invoice['ContactPerson']['FullName'])) {
+        if (!empty($invoice['ContactPerson']) && !empty($invoice['ContactPerson']['FullName'])) {
             $SalesInvoice .= "<ContactPerson><FullName>{$invoice['ContactPerson']['FullName']}</FullName></ContactPerson>";
         }
 
         // Invoice lines
-        if(!empty($invoice['InvoiceLines'])) {
+        if (!empty($invoice['InvoiceLines'])) {
             $InvoiceLines = [];
-            foreach($invoice['InvoiceLines'] as $InvoiceLine) {
+            foreach ($invoice['InvoiceLines'] as $InvoiceLine) {
                 $Line = '';
-                foreach(['ProductQuantity', 'LineAmount', 'LineVATAmount'] as $k) {
-                    if(!empty($InvoiceLine[$k])) $Line .= "<$k>{$InvoiceLine[$k]}</$k>";
+                foreach (['ProductQuantity', 'LineAmount', 'LineVATAmount'] as $k) {
+                    if (!empty($InvoiceLine[$k])) {
+                        $Line .= "<$k>{$InvoiceLine[$k]}</$k>";
+                    }
                 }
                 $Product = '';
                 foreach(['Description', 'SalesPrice', 'VATPercentage', 'VATType', 'GLAccountCode', 'Remarks'] as $k) {
-                    if(!empty($InvoiceLine['Product'][$k])) $Product .= "<$k>{$InvoiceLine['Product'][$k]}</$k>";
+                    if (!empty($InvoiceLine['Product'][$k])) {
+                        $Product .= "<$k>{$InvoiceLine['Product'][$k]}</$k>";
+                    }
                 }
                 $InvoiceLines[] = "<InvoiceLine>$Line<Product>$Product</Product></InvoiceLine>";
             }
@@ -103,15 +114,18 @@ class Yuki
         }
         // die($SalesInvoice);
         // XML doc, and send it
-        $xmlvar = new SoapVar('<ns1:xmlDoc><SalesInvoices xmlns="urn:xmlns:http://www.theyukicompany.com:salesinvoices" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><SalesInvoice>'.
-            $SalesInvoice.
-            '</SalesInvoice></SalesInvoices></ns1:xmlDoc>', XSD_ANYXML);
+        $xmlvar = new SoapVar(
+            '<ns1:xmlDoc><SalesInvoices xmlns="urn:xmlns:http://www.theyukicompany.com:salesinvoices" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><SalesInvoice>'
+                . $SalesInvoice
+                . '</SalesInvoice></SalesInvoices></ns1:xmlDoc>',
+            XSD_ANYXML
+        );
 
         $return = $this->ProcessSalesInvoices(['sessionId' => $this->sid, 'administrationId' => $this->aid, 'xmlDoc' => $xmlvar]);
 
         // Check the result to see whether it was succesful
         $result_xml = simplexml_load_string(current($return->ProcessSalesInvoicesResult));
-        if(!$result_xml->TotalSucceeded->__toString()) {
+        if (!$result_xml->TotalSucceeded->__toString()) {
             // None succeeded, so throw the error message
             throw new Exception($result_xml->Invoice->Message);
         }
@@ -131,15 +145,15 @@ class Yuki
 
     /**
      * Also called from constructor, if constructed with api key
-     * @param string $api_key Yuki API key
+     * @param string $apiKey Yuki API key
      * @throws Exception If api key is not set
      */
-    public function login($api_key) {
-        if (!$api_key) {
+    public function login($apiKey) {
+        if (!$apiKey) {
             throw new Exception('Yuki API key not set. Please check your company\'s settings. You can find or create a Yuki API key (of type Administration) under Settings > Webservices in Yuki.');
         }
 
-        $this->sid = $this->sid($api_key);
+        $this->sid = $this->sid($apiKey);
         $this->aid = $this->aid();
     }
 
@@ -154,9 +168,8 @@ class Yuki
         // Save and return the result
         try {
             $xml = simplexml_load_string(current($result->AdministrationsResult));
-            return $this->aid = $xml->Administration->attributes()['ID'];
-        }
-        catch(Exception $e) {
+            return $xml->Administration->attributes()['ID'];
+        } catch (Exception $e) {
             throw new Exception('Yuki authentication failed. The API key works, but it does not seem to have access to any Administration.');
         }
     }
@@ -172,9 +185,10 @@ class Yuki
 
         // Save and return the result
         if ($result && !empty($result->AuthenticateResult)) {
-            return $this->sid = $result->AuthenticateResult;
+            return $result->AuthenticateResult;
+        } else {
+            throw new Exception('Authentication failed. Please check your company\'s Yuki accessKey.');
         }
-        else throw new Exception('Authentication failed. Please check your company\'s Yuki accessKey.');
     }
 
     /**
@@ -195,13 +209,12 @@ class Yuki
      * @throws Exception
      */
     public function __call($method, $params) {
-        try{
+        try {
             $result = $this->soap->__soapCall($method, $params);
             return $result;
-        }
-        catch(Exception $e) {
+        } catch (Exception $e) {
             // rethrow with a little more information
-            throw new Exception("$method failed: [".$e->getCode().'] '.$e->getMessage(), $e->getCode());
+            throw new Exception("$method failed: [".$e->getCode().'] ' . $e->getMessage(), $e->getCode());
         }
     }
 
@@ -209,12 +222,14 @@ class Yuki
      * Yuki constructor (creates the SOAP client).
      *
      * @param null|string $apikey If provided, will immediately connect
-     *  @param null|string $wsdl 'sales' or 'accounting', if null then 'sales' to be backwards compatible with older version
+     * @param null|string $wsdl 'sales' or 'accounting' (if null then 'sales')
      * @throws Exception If the SOAP client could not be instantiated, or the login failed
      */
     public function __construct($apikey = null, $wsdl = null) {
         $this->soap = new SoapClient($this->getWSDL($wsdl), ['trace' => true]);
-        if($apikey) $this->login($apikey);
+        if ($apikey) {
+            $this->login($apikey);
+        }
     }
 
     /***
@@ -238,39 +253,57 @@ class Yuki
         }
     }
 
+    /**
+     * @param $start
+     * @param $end
+     * @return array
+     * @throws Exception
+     */
     public function GetAdministrationNetRevenue($start, $end){
-        try{
-            return $this->NetRevenue(['sessionID' => $this->sid, 'administrationID' => $this->aid,'StartDate' => $start, 'EndDate' => $end]);
-        }
-        catch(Exception $e){
-            throw new Exception('Could not retrieve Net Revenue for adminstration ' . $this->aid . ' from ' . $start . ' until ' .$end);
+        try {
+            return $this->NetRevenue(['sessionID' => $this->sid, 'administrationID' => $this->aid, 'StartDate' => $start, 'EndDate' => $end]);
+        } catch (Exception $e) {
+            throw new Exception('Could not retrieve Net Revenue for administration ' . $this->aid . ' from ' . $start . ' to ' . $end);
         }
     }
 
+    /**
+     * @param $date
+     * @return array
+     * @throws Exception
+     */
     public function GetAccountBalance($date){
-        try{
+        try {
             return $this->GLAccountBalance(['sessionID' => $this->sid, 'administrationID' => $this->aid, 'transactionDate' => $date]);
-        }
-        catch(Exception $e){
-            throw new Exception('Could not retrieve AccountBalance for adminstration ' . $this->aid . ' and date: ' .$date);
+        } catch (Exception $e) {
+            throw new Exception('Could not retrieve AccountBalance for administration ' . $this->aid . ' on date: ' . $date);
         }
     }
 
+    /**
+     * @return array
+     * @throws Exception
+     */
     public function GetAccountCodes(){
-        try{
+        try {
             return $this->GetGLAccountScheme(['sessionID' => $this->sid, 'administrationID' => $this->aid]);
-        }
-        catch(Exception $e){
-            throw new Exception('Could not retrieve AccountCodes for adminstration ' . $this->aid . ' and session: ' . $this->sid);
+        } catch (Exception $e) {
+            throw new Exception('Could not retrieve AccountCodes for administration ' . $this->aid . ' and session: ' . $this->sid);
         }
     }
 
-    public function GetTransactions($code, $start, $end){
-        try{
-            return $this->GLAccountTransactions(['sessionID' => $this->sid, 'administrationID' => $this->aid, 'GLAccountCode' => $code,'StartDate' => $start, 'EndDate' => $end]);
-        }
-        catch(Exception $e){
-            throw new Exception('Could not retrieve Transactions for adminstration ' . $this->aid . ' and accountcode: ' .$code. ' from ' . $start . ' until ' .$end);
+    /**
+     * @param $accountCode
+     * @param $start
+     * @param $end
+     * @return array
+     * @throws Exception
+     */
+    public function GetTransactions($accountCode, $start, $end){
+        try {
+            return $this->GLAccountTransactions(['sessionID' => $this->sid, 'administrationID' => $this->aid, 'GLAccountCode' => $accountCode, 'StartDate' => $start, 'EndDate' => $end]);
+        } catch (Exception $e) {
+            throw new Exception('Could not retrieve Transactions for administration ' . $this->aid . ' and accountcode: ' . $accountCode . ' from ' . $start . ' to ' . $end);
         }
     }
 }
